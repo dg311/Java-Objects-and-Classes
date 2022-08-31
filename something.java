f0e4ff47e9ce8eae145fc6eadf9a4a0a52ede7e5
dg311 <83474866+dg311@users.noreply.github.com>
2022-08-31 11:23:07 -0400

    Create something.java

---
+++ something.java
@@ -0,0 +1,25 @@
+public class Main {
+    String doesItWork = "Can you see this text? Does my code work?";
+    int age = 11;
+    String myHobbies = "I like basketball, tennis, coding, math, and chess.";
+    String placesILikeToGo = "I like to go to arcades, tennis courts, and live sports matches.";
+    String whatILikeToDoAtHome = "I like to code, play chess, and watch TV.";
+    boolean areTheseFactsTrue = true;
+    int hobbyNumbers = 5;
+    int placeNumbers = 3;
+    int homeActivityNumbers = 3;
+    boolean areTheNumbersRight = true;
+    public static void main(String[]args) {
+        Main myFacts = new Main();
+        System.out.println(myFacts.doesItWork);
+        System.out.println(myFacts.age);
+        System.out.println(myFacts.myHobbies);
+        System.out.println(myFacts.placesILikeToGo);
+        System.out.println(myFacts.whatILikeToDoAtHome);
+        System.out.println(myFacts.areTheseFactsTrue);
+        System.out.println(myFacts.hobbyNumbers);
+        System.out.println(myFacts.placeNumbers);
+        System.out.println(myFacts.homeActivityNumbers);
+        System.out.println(myFacts.areTheNumbersRight);
+    }
+}
